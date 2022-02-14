3ac5ef93b5a975e50569226d647b0eeedcc1678f
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2022-02-14 11:21:52 +0100

    observer notification conditional on add/remove

--- app/src/test/java/org/thoughtcrime/securesms/trustedIntroductions/TrustedIntroductionContactsViewModelTest.kt
+++ app/src/test/java/org/thoughtcrime/securesms/trustedIntroductions/TrustedIntroductionContactsViewModelTest.kt
@@ -1,7 +1,21 @@
 package org.thoughtcrime.securesms.trustedIntroductions
 
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class TrustedIntroductionContactsViewModelTest {
+  @Test
+  fun `is Singleton`(){
+    val factory:  TrustedIntroductionContactsViewModel.Factory = TrustedIntroductionContactsViewModel.Factory(FakeRecipients.recipientIDs[0])
+    val viewModel: TrustedIntroductionContactsViewModel = ViewModelProvider(Mockito.mock(ViewModelStoreOwner::class.java), factory).get(TrustedIntroductionContactsViewModel::class.java)
 
+
+  }
 }
