0e4d2f47c926ea74c0db221fa03c0d0ad5eca2a7
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-02-02 02:21:48 -0800

    uncommented assertion errors for fast fail

--- app/src/main/java/org/thoughtcrime/securesms/jobs/TrustedIntroductionsReceiveJob.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobs/TrustedIntroductionsReceiveJob.java
@@ -121,7 +121,7 @@ public class TrustedIntroductionsReceiveJob extends BaseJob  {
       long result = db.incomingIntroduction(introduction);
       if (result == -1){
         // TODO: How to fail gracefully?
-        //throw new AssertionError(TAG + String.format("Introduction insertion for %s failed...", introduction.getIntroduceeName()));
+        throw new AssertionError(TAG + String.format("Introduction insertion for %s failed...", introduction.getIntroduceeName()));
       }
       inserts_succeeded++;
       // Testing
@@ -152,7 +152,7 @@ public class TrustedIntroductionsReceiveJob extends BaseJob  {
         } catch (JSONException | NullPointerException e) {
           e.printStackTrace();
           // TODO: fail gracefully
-          //throw new AssertionError("JSON deserialization of introductions failed!");
+          throw new AssertionError("JSON deserialization of introductions failed!");
         }
       }
 
