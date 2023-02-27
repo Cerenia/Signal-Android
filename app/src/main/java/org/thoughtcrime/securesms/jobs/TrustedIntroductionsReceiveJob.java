2084fd2ac79a6864e49d8f975b6226e1237db5bd
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-02-27 11:41:17 +0100

    retry params to test notification codepath

--- app/src/main/java/org/thoughtcrime/securesms/jobs/TrustedIntroductionsReceiveJob.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobs/TrustedIntroductionsReceiveJob.java
@@ -111,6 +111,9 @@ public class TrustedIntroductionsReceiveJob extends BaseJob  {
 
 
   @Override protected void onRun() throws Exception {
+    // TODO: testing notification
+    throw new AssertionError("Testing notification codepath");
+    /*
     if(!bodyParsed){
       List<TI_Data> tiData = parseTIMessage(messageBody, timestamp, introducerId);
       introductions.addAll(tiData);
@@ -126,6 +129,7 @@ public class TrustedIntroductionsReceiveJob extends BaseJob  {
       inserts_succeeded++;
     }
     Log.e(TAG, "TrustedIntroductionsReceiveJob completed!");
+    */
   }
 
   // TODO
