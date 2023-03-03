0568096c7855a389d8467bb7a7f22e58bd125a9c
Cody Henthorne <cody@signal.org>
2023-03-03 11:42:30 -0500

    Add message processing performance test.

--- app/src/main/java/org/thoughtcrime/securesms/messages/MessageContentProcessor.java
+++ app/src/main/java/org/thoughtcrime/securesms/messages/MessageContentProcessor.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import com.annimon.stream.Collectors;
 import com.annimon.stream.Stream;
@@ -200,7 +201,7 @@ import static org.thoughtcrime.securesms.trustedIntroductions.TI_Utils.handleTIM
  * data to our data stores.
  */
 @SuppressWarnings({ "OptionalGetWithoutIsPresent", "OptionalIsPresent" })
-public final class MessageContentProcessor {
+public class MessageContentProcessor {
 
   private static final String TAG = Log.tag(MessageContentProcessor.class);
 
@@ -210,7 +211,8 @@ public final class MessageContentProcessor {
     return new MessageContentProcessor(context);
   }
 
-  private MessageContentProcessor(@NonNull Context context) {
+  @VisibleForTesting
+  MessageContentProcessor(@NonNull Context context) {
     this.context = context;
   }
 
