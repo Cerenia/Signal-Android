2e19fc3ebc910022cd4a09d8fb0f993ae9e91918
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-04-20 10:09:14 +0200

    Revert "Ignore broken story unit tests."
    This reverts commit eb57845138c91af300c0ae1efc5ee382e47d4646.

--- app/src/test/java/org/thoughtcrime/securesms/stories/viewer/StoryViewerViewModelTest.kt
+++ app/src/test/java/org/thoughtcrime/securesms/stories/viewer/StoryViewerViewModelTest.kt
@@ -6,7 +6,6 @@ import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
@@ -23,7 +22,6 @@ import org.thoughtcrime.securesms.recipients.RecipientId
 import org.thoughtcrime.securesms.stories.Stories
 import org.thoughtcrime.securesms.stories.StoryViewerArgs
 
-@Ignore
 class StoryViewerViewModelTest {
 
   @Rule
