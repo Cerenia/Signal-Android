ac19d3f5b102625c4dbe4815219e8bceb964150b
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-04-20 10:09:55 +0200

    Revert "Debounce thread updates for incoming messages."
    This reverts commit 0e1a13816b9944ceadde6197b7d96467006613b7.

--- app/src/main/java/org/thoughtcrime/securesms/jobs/ThreadUpdateJob.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobs/ThreadUpdateJob.java
@@ -9,17 +9,11 @@ import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
 import org.thoughtcrime.securesms.jobmanager.JsonJobData;
 import org.thoughtcrime.securesms.jobmanager.Job;
 
-/**
- * A job that effectively debounces thread updates through a combination of having a max instance count
- * and sleeping at the end of the job to make sure it takes a minimum amount of time.
- */
 public final class ThreadUpdateJob extends BaseJob {
 
   public static final String KEY = "ThreadUpdateJob";
 
   private static final String KEY_THREAD_ID = "thread_id";
-
-  private static final long DEBOUNCE_INTERVAL = 3000;
 
   private final long threadId;
 
@@ -53,7 +47,7 @@ public final class ThreadUpdateJob extends BaseJob {
   @Override
   protected void onRun() throws Exception {
     SignalDatabase.threads().update(threadId, true);
-    ThreadUtil.sleep(DEBOUNCE_INTERVAL);
+    ThreadUtil.sleep(1000);
   }
 
   @Override
