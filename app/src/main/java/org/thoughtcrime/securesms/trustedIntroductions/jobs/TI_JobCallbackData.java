43509d6ae133d7ffac33462a30c52924f37878c2
Cerenia <christelle.gloor@inf.ethz.ch>
2024-05-31 12:16:27 +0200

    Commit before cloning branch TI_migration_v7.5.2 of subrepository.

--- app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions/jobs/TI_JobCallbackData.java
+++
@@ -1,8 +0,0 @@
-package org.thoughtcrime.securesms.trustedIntroductions.jobs;
-
-import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
-
-public abstract class TI_JobCallbackData implements TI_Serialize {
-  public abstract TrustedIntroductionsRetreiveIdentityJob.TI_RetrieveIDJobResult getIdentityResult();
-  abstract public TI_Data getIntroduction();
-}
