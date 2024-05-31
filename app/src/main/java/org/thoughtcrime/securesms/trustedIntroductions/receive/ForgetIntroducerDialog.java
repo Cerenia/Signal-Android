88119de78ce929b3ce281e0f54c28db0b6a69a0a
Cerenia <christelle.gloor@inf.ethz.ch>
2024-05-31 12:16:32 +0200

    git subrepo clone --branch=TI_migration_v7.5.2 --force git@github.com:Cerenia/trusted-introductions.git app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions
    subrepo:
      subdir:   "app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions"
      merged:   "8e472ae8fd"
    upstream:
      origin:   "git@github.com:Cerenia/trusted-introductions.git"
      branch:   "TI_migration_v7.5.2"
      commit:   "8e472ae8fd"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions/receive/ForgetIntroducerDialog.java
@@ -0,0 +1,42 @@
+package org.thoughtcrime.securesms.trustedIntroductions.receive;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import org.signal.core.util.logging.Log;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Utils;
+
+import java.util.Date;
+
+import static org.thoughtcrime.securesms.trustedIntroductions.TI_Utils.INTRODUCTION_DATE_PATTERN;
+
+/**
+ * Asks user if they really want to forget who made an introduction.
+ */
+public final class ForgetIntroducerDialog {
+
+    private static final String TAG = String.format(TI_Utils.TI_LOG_TAG, Log.tag(ForgetIntroducerDialog.class));
+
+    public interface ForgetIntroducer {
+        void forgetIntroducer(@NonNull Long introductionId);
+    }
+
+    private ForgetIntroducerDialog() {
+    }
+
+    public static void show(@NonNull Context context, @NonNull Long introductionId, @NonNull String introduceeName, @NonNull String introducerName, @NonNull Date date, ForgetIntroducer f) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context).setTitle(R.string.ForgetIntroucerDialog__Title);
+        // TODO: do we still want to differentiate? or can we get rid of t?
+        String text = context.getString(R.string.ForgetIntroucerDialog__Forget_Introducer_ALL, introducerName, introduceeName, INTRODUCTION_DATE_PATTERN.format(date));
+        builder.setMessage(text);
+        builder.setNegativeButton(android.R.string.no, (dialog, which) -> dialog.dismiss())
+               .setPositiveButton(R.string.ForgetIntroucerDialog__forget, (dialog, which) -> {
+                   dialog.dismiss();
+                   f.forgetIntroducer(introductionId);
+               });
+        builder.show();
+    }
+}
---
+++ app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions/receive/ManageManager.java
@@ -0,0 +1,71 @@
+package org.thoughtcrime.securesms.trustedIntroductions.receive;
+
+import androidx.annotation.NonNull;
+import androidx.core.util.Consumer;
+import androidx.core.util.Pair;
+
+import org.signal.core.util.concurrent.SignalExecutors;
+import org.signal.core.util.logging.Log;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Utils;
+import org.thoughtcrime.securesms.trustedIntroductions.glue.TI_DatabaseGlue;
+import org.thoughtcrime.securesms.trustedIntroductions.database.TI_Database;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.webrtc.ContextUtils.getApplicationContext;
+
+public class ManageManager {
+
+  private static final String TAG =  String.format(TI_Utils.TI_LOG_TAG, Log.tag(ManageManager.class));
+
+  @NonNull private final String      forgottenPlaceholder;
+  // Dependency injection
+  private final          TI_DatabaseGlue tdb;
+
+  ManageManager(@NonNull TI_DatabaseGlue tdb, @NonNull String forgottenPlaceholder){
+    this.tdb = tdb;
+    this.forgottenPlaceholder = forgottenPlaceholder;
+  }
+
+  void getIntroductions(@NonNull Consumer<List<Pair<TI_Data, ManageViewModel.IntroducerInformation>>> listConsumer){
+    SignalExecutors.BOUNDED.execute(() -> {
+
+      // Pull introductions out of the database
+      TI_Database.IntroductionReader reader        = tdb.getAllDisplayableIntroductions();
+      ArrayList<TI_Data>             introductions = new ArrayList<>();
+      while(reader.hasNext()){
+        introductions.add(reader.getNext());
+      }
+      // sort by date
+      Collections.sort(introductions, Comparator.comparing(TI_Data::getTimestamp));
+      ArrayList<Pair<TI_Data, ManageViewModel.IntroducerInformation>> result = new ArrayList<>();
+      ManageViewModel.IntroducerInformation i = null;
+      for (TI_Data d: introductions) {
+        if(d.getIntroducerServiceId() == null){
+          i = new ManageViewModel.IntroducerInformation(forgottenPlaceholder, forgottenPlaceholder);
+        } else {
+          try {
+            Recipient r      = Recipient.live(TI_Utils.getRecipientIdOrUnknown(d.getIntroducerServiceId())).resolve();
+            String    number = r.getE164().orElse("");
+            // TODO: using getApplication context because the context doesn't matter... (22-10-06)
+            // It just circularly gets passed around between methods in the Recipient but is never used for anything.
+            i = new ManageViewModel.IntroducerInformation(r.getDisplayNameOrUsername(getApplicationContext()), number);
+          } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
+          }
+        } // TODO: this should not happen
+        if (i != null) {
+          result.add(new Pair<>(d, i));
+        }
+      }
+      listConsumer.accept(result);
+    });
+  }
+
+}
