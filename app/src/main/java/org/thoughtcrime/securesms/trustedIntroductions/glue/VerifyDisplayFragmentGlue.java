c74023abe1d733b13345a94656916f8f9ee440c7
Cerenia <christelle.gloor@inf.ethz.ch>
2024-06-21 18:55:56 +0200

    git subrepo clone --branch=TI_migration_v7.8.1 --force git@github.com:Cerenia/trusted-introductions.git app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions
    subrepo:
      subdir:   "app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions"
      merged:   "2068f8efc6"
    upstream:
      origin:   "git@github.com:Cerenia/trusted-introductions.git"
      branch:   "TI_migration_v7.8.1"
      commit:   "2068f8efc6"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions/glue/VerifyDisplayFragmentGlue.java
@@ -0,0 +1,131 @@
+package org.thoughtcrime.securesms.trustedIntroductions.glue;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+
+import org.signal.core.util.concurrent.SignalExecutors;
+import org.signal.core.util.logging.Log;
+import org.signal.libsignal.protocol.IdentityKey;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.crypto.ReentrantSessionLock;
+import org.thoughtcrime.securesms.database.IdentityTable;
+import org.thoughtcrime.securesms.database.SignalDatabase;
+import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
+import org.thoughtcrime.securesms.jobs.MultiDeviceVerifiedUpdateJob;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.recipients.RecipientId;
+import org.thoughtcrime.securesms.storage.StorageSyncHelper;
+import org.thoughtcrime.securesms.trustedIntroductions.ClearVerificationDialog;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Utils;
+import org.thoughtcrime.securesms.trustedIntroductions.database.TI_IdentityTable;
+import org.thoughtcrime.securesms.util.IdentityUtil;
+import org.thoughtcrime.securesms.util.ViewUtil;
+import org.thoughtcrime.securesms.verify.VerifyDisplayFragment;
+import org.whispersystems.signalservice.api.SignalSessionLock;
+
+public interface VerifyDisplayFragmentGlue {
+
+  final String VERIFIED_STATE  = "verified_state";
+  final String TAG_TI = String.format(TI_Utils.TI_LOG_TAG, Log.tag(VerifyDisplayFragment.class));
+
+  static void initializeVerifyButton(boolean verified, Button verifyButton, RecipientId recipientId, androidx.fragment.app.FragmentActivity activity, IdentityKey remoteIdentity){
+    updateVerifyButtonText(verified, verifyButton);
+    verifyButton.setOnClickListener((button -> updateVerifyButtonLogic((Button)button, recipientId, activity, remoteIdentity)));
+  }
+
+  static void extendBundle(Bundle extras, boolean verifiedState){
+    extras.putBoolean(VERIFIED_STATE, verifiedState);
+  }
+
+  static void updateVerifyButtonText(boolean verified, Button verifyButton) {
+    if (verified) {
+      verifyButton.setText(R.string.verify_display_fragment__clear_verification);
+    } else {
+      verifyButton.setText(R.string.verify_display_fragment__mark_as_verified);
+    }
+  }
+
+  static void updateVerifyButtonLogic(Button verifyButton, RecipientId recipientId, androidx.fragment.app.FragmentActivity activity, IdentityKey remoteIdentity) {
+    // TODO: This needs a good refactoring since I want to be close to the original. I completely mangled this class.
+    // Check the current verification status
+    TI_IdentityTable.VerifiedStatus previousStatus = SignalDatabase.tiIdentityDatabase().getVerifiedStatus(recipientId);
+    Log.i(TAG_TI, "Saving identity: " + recipientId);
+    if (TI_IdentityTable.VerifiedStatus.stronglyVerified(previousStatus)) {
+      // TODO: when would this activity ever be null?
+      if (activity != null) {
+        // go through user check first.
+        ClearVerificationDialog.show(activity, previousStatus, recipientId, remoteIdentity, verifyButton);
+      }
+    } else if (previousStatus == TI_IdentityTable.VerifiedStatus.MANUALLY_VERIFIED) {
+      // manually verified, no user check necessary
+      TI_Utils.updateContactsVerifiedStatus(recipientId, remoteIdentity, TI_IdentityTable.VerifiedStatus.UNVERIFIED);
+      updateVerifyButtonText(false, verifyButton);
+    } else {
+      // Unverified or default, simply set to manually verified
+      TI_Utils.updateContactsVerifiedStatus(recipientId, remoteIdentity, TI_IdentityTable.VerifiedStatus.MANUALLY_VERIFIED);
+      updateVerifyButtonText(true, verifyButton);
+    }
+  }
+
+  static void onSuccessfullVerification(RecipientId recipientId, IdentityKey remoteIdentity, Button verifyButton){
+    // The fingerprint matched after a QR scann and we can update the users verification status
+    TI_Utils.updateContactsVerifiedStatus(recipientId, remoteIdentity, TI_IdentityTable.VerifiedStatus.DIRECTLY_VERIFIED);
+    updateVerifyButtonText(true, verifyButton);
+  }
+
+  private void updateContactsVerifiedStatus(TI_IdentityTable.VerifiedStatus status, Recipient recipient, IdentityKey remoteIdentity, androidx.fragment.app.FragmentActivity activity) {
+    final RecipientId recipientId = recipient.getId();
+    Log.i(TAG_TI, "Saving identity: " + recipientId);
+    SignalExecutors.BOUNDED.execute(() -> {
+      try (SignalSessionLock.Lock unused = ReentrantSessionLock.INSTANCE.acquire()) {
+        final boolean verified = TI_IdentityTable.VerifiedStatus.isVerified(status);
+        if (verified) {
+          Log.i(TAG_TI, "Saving identity: " + recipientId);
+          ApplicationDependencies.getProtocolStore().aci().identities()
+                                 .saveIdentityWithoutSideEffects(recipientId,
+                                                                 remoteIdentity,
+                                                                 TI_IdentityTable.VerifiedStatus.toVanilla(status),
+                                                                 false,
+                                                                 System.currentTimeMillis(),
+                                                                 true);
+        } else {
+          ApplicationDependencies.getProtocolStore().aci().identities().setVerified(recipientId, remoteIdentity, TI_IdentityTable.VerifiedStatus.toVanilla(status));
+        }
+
+        // For other devices but the Android phone, we map the finer statusses to verified or unverified.
+        ApplicationDependencies.getJobManager()
+                               .add(new MultiDeviceVerifiedUpdateJob(recipientId,
+                                                                     remoteIdentity,
+                                                                     TI_IdentityTable.VerifiedStatus.toVanilla(status)));
+        StorageSyncHelper.scheduleSyncForDataChange();
+        IdentityUtil.markIdentityVerified(activity, recipient, verified, false);
+      }
+    });
+  }
+
+  // TODO: Create a PR for this instead of keeping the change in your codebase. Should be a small enough change to get through.
+  /**static void wrapAnimationInTryCatch(){
+    // Insert at onAnimationEnd
+    try{
+      ScaleAnimation scaleAnimation1 = new ScaleAnimation(1, 0, 1, 0,
+                                                          ScaleAnimation.RELATIVE_TO_SELF, 0.5f,
+                                                          ScaleAnimation.RELATIVE_TO_SELF, 0.5f);
+
+      scaleAnimation1.setInterpolator(new AnticipateInterpolator());
+      scaleAnimation1.setDuration(500);
+      ViewUtil.animateOut(qrVerified, scaleAnimation1, View.GONE);
+      ViewUtil.fadeIn(qrCode, 800);
+      qrCodeContainer.setEnabled(true);
+      tapLabel.setText(getString(R.string.verify_display_fragment__tap_to_scan));
+    } catch (IllegalStateException e){
+      // TODO: Maybe create PR for that at some point? Not really problem in normal operation, I encounter that because I am
+      // verifying lots of contacts fast by hand
+      Log.e(TAG, "Illegal state! Possibly the user navigated back before the feedback was posted.");
+      e.printStackTrace();
+      // simply do nothing in this case
+    }
+  }**/
+}
---
+++ app/src/main/java/org/thoughtcrime/securesms/trustedIntroductions/receive/ManageListFragment.java
@@ -0,0 +1,417 @@
+package org.thoughtcrime.securesms.trustedIntroductions.receive;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.RecyclerView;
+import static org.thoughtcrime.securesms.trustedIntroductions.TI_Utils.splitIntroductionDate;
+import static org.thoughtcrime.securesms.trustedIntroductions.receive.ManageActivity.ActiveTab.NEW;
+
+import com.google.android.material.button.MaterialButton;
+import com.pnikosis.materialishprogress.ProgressWheel;
+
+import org.signal.core.util.logging.Log;
+import org.thoughtcrime.securesms.trustedIntroductions.database.TI_Database;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Data;
+import org.thoughtcrime.securesms.R;
+import org.thoughtcrime.securesms.trustedIntroductions.TI_Utils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+import androidx.tracing.Trace;
+
+public class ManageListFragment extends Fragment implements DeleteIntroductionDialog.DeleteIntroduction, ForgetIntroducerDialog.ForgetIntroducer{
+
+  private static final String TAG = String.format(TI_Utils.TI_LOG_TAG, Log.tag(ManageListFragment.class));
+
+  // TODO: Needed?
+  private ProgressWheel showIntroductionsProgress;
+  private ManageViewModel viewModel;
+  private ManageAdapter adapter;
+  private TextView no_introductions;
+  private View                           all_header;
+  private ManageActivity.ActiveTab tab = NEW;
+  private MaterialButton showConflicting;
+  // Don't refresh list 5 times on setup.
+  private boolean sCisFirstInit = true;
+
+  private MaterialButton showAccepted;
+  private boolean sAisFirstInit = true;
+
+  private MaterialButton showRejected;
+  private boolean sRisFirstInit = true;
+
+  private MaterialButton showStale;
+  private boolean sSisFirstInit = true;
+
+  // Because final onCreate in AppCompat dissalows me from using a Fragment Factory, I need to use a Bundle for Arguments.
+  static String TYPE_KEY = "type_key";
+
+  static String FORGOTTEN_INTRODUCER;
+
+  public ManageListFragment(){
+    super();
+  }
+
+  public ManageListFragment(@NonNull ViewModelStoreOwner owner, @NonNull ManageActivity.ActiveTab type){
+    this.tab = type;
+  }
+
+  @Override
+  public void onCreate(Bundle b){
+    super.onCreate(b);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                           @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.ti_manage_fragment, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+    super.onViewCreated(view, savedInstanceState);
+    ManageViewModel.Factory factory = new ManageViewModel.Factory(FORGOTTEN_INTRODUCER);
+    viewModel = new ViewModelProvider(getActivity(), factory).get(ManageViewModel.class);
+    if(!viewModel.introductionsLoaded()){
+      viewModel.loadIntroductions();
+    }
+    adapter = new ManageAdapter(requireContext(), new IntroductionClickListener(this, this));
+    RecyclerView introductionList = view.findViewById(R.id.recycler_view);
+    introductionList.setClipToPadding(true);
+    introductionList.setAdapter(adapter);
+    no_introductions = view.findViewById(R.id.no_introductions_found);
+    all_header = view.findViewById(R.id.manage_fragment_header);
+    // Filter state
+    showConflicting = view.findViewById(R.id.conflictingFilter);
+    showStale = view.findViewById(R.id.staleFilter);
+    showAccepted = view.findViewById(R.id.acceptedFilter);
+    showRejected = view.findViewById(R.id.rejectedFilter);
+    if(savedInstanceState != null && savedInstanceState.getString(TYPE_KEY)!=null){
+      tab = ManageActivity.ActiveTab.fromString(savedInstanceState.getString(TYPE_KEY));
+    }
+    showConflicting.setVisibility(View.VISIBLE);
+    showStale.setVisibility(View.VISIBLE);
+    if(viewModel != null){
+      // viewModel has default values.
+      showConflicting.setChecked(viewModel.showConflicting().getValue());
+      showStale.setChecked(viewModel.showStale().getValue());
+    } else {
+      showConflicting.setChecked(true);
+      showStale.setChecked(true);
+    }
+    showConflicting.addOnCheckedChangeListener((button, isChecked) ->  {
+      viewModel.setShowConflicting(isChecked);
+    });
+    showStale.addOnCheckedChangeListener((button, isChecked) ->{
+      viewModel.setShowStale(isChecked);
+    });
+    switch(tab){
+      case NEW:
+        // Accepted, Rejected and stale don't show up
+        showAccepted.setVisibility(View.GONE);
+        showRejected.setVisibility(View.GONE);
+        showStale.setVisibility(View.GONE);
+        break;
+      case LIBRARY:
+        showAccepted.setVisibility(View.VISIBLE);
+        showRejected.setVisibility(View.VISIBLE);
+        if(viewModel != null){
+          showAccepted.setChecked(viewModel.showAccepted().getValue());
+          showRejected.setChecked(viewModel.showRejected().getValue());
+        } else {
+          showAccepted.setChecked(true);
+          showRejected.setChecked(true);
+        }
+        showAccepted.addOnCheckedChangeListener((button, isChecked) ->{
+          viewModel.setShowAccepted(isChecked);
+        });
+        showRejected.addOnCheckedChangeListener((button, isChecked) ->{
+          viewModel.setShowRejected(isChecked);
+        });
+    }
+    // Filter state Obvservers
+    viewModel.showConflicting().observe(getViewLifecycleOwner(), state ->{
+      sCisFirstInit = onFilterStateChanged(showConflicting, state, sCisFirstInit);
+    });
+    viewModel.showStale().observe(getViewLifecycleOwner(), state ->{
+      sSisFirstInit = onFilterStateChanged(showStale, state, sSisFirstInit);
+    });
+    viewModel.showAccepted().observe(getViewLifecycleOwner(), state ->{
+      sAisFirstInit = onFilterStateChanged(showAccepted, state, sAisFirstInit);
+    });
+    viewModel.showRejected().observe(getViewLifecycleOwner(), state->{
+      sRisFirstInit = onFilterStateChanged(showRejected, state, sRisFirstInit);
+    });
+    // Introduction Observer
+    this.viewModel.getIntroductions().observe(getViewLifecycleOwner(), introductions -> {
+      // Screen layout
+      if(introductions.size() > 0){
+        no_introductions.setVisibility(View.GONE);
+        all_header.setVisibility(View.VISIBLE);
+      } else {
+        no_introductions.setVisibility(View.VISIBLE);
+        all_header.setVisibility(View.GONE);
+        no_introductions.setText(R.string.ManageIntroductionsFragment__No_Introductions_all);
+      }
+      refreshList();
+    });
+  }
+
+  /**
+   *
+   * @param b which button to set.
+   * @param newCheckState the new newCheckState.
+   * @param isFirstInit global indicating if the button was ever set before.
+   * @return false, the value to be assigned to the global xXisFirstInit of the button if the method succeeds.
+   */
+  private boolean onFilterStateChanged(MaterialButton b, Boolean newCheckState, Boolean isFirstInit){
+    if(b.isChecked() != newCheckState){
+      b.setChecked(newCheckState);
+    }
+    if(!isFirstInit){
+      refreshList();
+    }
+    return false;
+  }
+
+  @Override public void onSaveInstanceState(@NonNull Bundle outState) {
+    outState.putString(TYPE_KEY, tab.toString());
+    super.onSaveInstanceState(outState);
+  }
+
+  /**
+   * Decides if this datum must be displayed in the fragment. Depends on tabtype and active filters.
+   * @param p the datum to be evaluated.
+   * @return true if displayed, false if not.
+   */
+  private boolean isDisplayed(Pair<TI_Data, ManageViewModel.IntroducerInformation> p){
+    TI_Database.State s = p.first.getState();
+    switch(tab){
+      case NEW:
+        // Only display pending and conflicting
+        if(!(s == TI_Database.State.PENDING || s == TI_Database.State.CONFLICTING)){
+          return false;
+        }
+        if(userFiltered(s)){
+          return false;
+        }
+        break;
+      case LIBRARY:
+        // Display everything but pending
+        if((s == TI_Database.State.PENDING)){
+          return false;
+        }
+        if(userFiltered(s)){
+          return false;
+        }
+        break;
+      default:
+        // fail open
+        return true;
+    }
+    return true;
+  }
+
+  /**
+   * Checks the state of the introduction against the user filters.
+   * @return true if filtered, false otherwise
+   */
+  private boolean userFiltered(TI_Database.State s){
+    switch(s){
+      case ACCEPTED:
+        if(!viewModel.showAccepted().getValue()){
+          return true;
+        }
+        break;
+      case REJECTED:
+        if(!viewModel.showRejected().getValue()){
+          return true;
+        }
+        break;
+      case CONFLICTING:
+        if(!viewModel.showConflicting().getValue()){
+          return true;
+        }
+        break;
+      case STALE_PENDING:
+        if(!viewModel.showStale().getValue()){
+          return true;
+        }
+        break;
+      case STALE_REJECTED:
+        if(!viewModel.showStale().getValue() && !viewModel.showStale().getValue()){
+          return true;
+        }
+        break;
+      case STALE_ACCEPTED:
+        if(!viewModel.showAccepted().getValue() && !viewModel.showStale().getValue()){
+          return true;
+        }
+        break;
+      case STALE_CONFLICTING:
+        if(!viewModel.showConflicting().getValue() && !viewModel.showStale().getValue()){
+          return true;
+        }
+        break;
+      default: // fail open
+        return false;
+    }
+    return false;
+  }
+
+  /**
+   * Filters complete introduction list by Tab type, search filter, button selectors and finally sorts.
+   * @param introductions All introductions returned by ViewModel.
+   * @param filter The user provided filter.
+   * @return the filtered list appropriate for the fragment depending on tab, button selectors and filter.
+   */
+  private List<Pair<TI_Data, ManageViewModel.IntroducerInformation>> getFiltered(List<Pair<TI_Data, ManageViewModel.IntroducerInformation>> introductions, @Nullable String filter){
+    List<Pair<TI_Data, ManageViewModel.IntroducerInformation>> filtered = new ArrayList<>();
+    for (Pair<TI_Data, ManageViewModel.IntroducerInformation> p: introductions) {
+      if(isDisplayed(p)){
+        filtered.add(p);
+      }
+    }
+    if(filter != null){
+      if(!filter.isEmpty() && filter.compareTo("") != 0){
+        Pattern filterPattern = Pattern.compile("\\A" + filter + ".*", Pattern.CASE_INSENSITIVE);
+        for (Pair<TI_Data, ManageViewModel.IntroducerInformation> p: introductions){
+          TI_Data                     d = p.first;
+          TI_Utils.TimestampDateParts timestampParts = splitIntroductionDate(d.getTimestamp());
+          // Split up for debugging. May be one big expr.
+          boolean matchYear = filterPattern.matcher(timestampParts.year).find();
+          boolean matchMonth = filterPattern.matcher(timestampParts.month).find();
+          boolean matchDay = filterPattern.matcher(timestampParts.day).find();
+          boolean matchHours = filterPattern.matcher(timestampParts.hours).find();
+          boolean matchMinutes = filterPattern.matcher(timestampParts.minutes).find();
+          boolean matchSeconds = filterPattern.matcher(timestampParts.seconds).find();
+          boolean matchIntroduceeName = filterPattern.matcher(d.getIntroduceeName()).find();
+          boolean matchIntroduceeNumber = filterPattern.matcher(d.getIntroduceeNumber()).find();
+          boolean matchIntroducerName = filterPattern.matcher(p.second.name).find();
+          boolean matchIntroducerNumber = filterPattern.matcher(p.second.number).find();
+          if (!(matchYear || matchMonth || matchDay || matchHours || !matchMinutes || matchSeconds || matchIntroduceeName || matchIntroduceeNumber
+                || matchIntroducerName || matchIntroducerNumber)){
+            filtered.remove(p);
+          }
+        }
+      }
+    }
+    return sortIntroductions(filtered);
+  }
+
+  private static Function<Pair<TI_Data, ManageViewModel.IntroducerInformation>, Long> dateExtractor = p -> p.first.getTimestamp();
+  private static Function<Pair<TI_Data, ManageViewModel.IntroducerInformation>, String> introduceeNameExtractor = p -> p.first.getIntroduceeName();
+  private static Function<Pair<TI_Data, ManageViewModel.IntroducerInformation>, Long> stateExtractor = p -> (long)p.first.getState().toInt();
+
+  /**
+   * Sorts introductions depending on the tab type.
+   * NEW: by date
+   * LIBRARY: by introducee
+   * ALL: by introducer
+   * The sorted list.
+   */
+  private List<Pair<TI_Data, ManageViewModel.IntroducerInformation>> sortIntroductions(List<Pair<TI_Data, ManageViewModel.IntroducerInformation>> filtered){
+    switch(tab){
+      case NEW:
+        filtered.sort(Comparator.comparing(dateExtractor));
+        return filtered;
+      case LIBRARY:
+        // First by state, then introducee, then date
+        filtered.sort(Comparator.comparing(stateExtractor).thenComparing(introduceeNameExtractor).thenComparing(dateExtractor));
+        return filtered;
+      default:
+        throw new AssertionError(TAG +"Unknown tab type!");
+    }
+  }
+
+  void refreshList(){
+    Trace.beginSection("refreshList:" + tab.toString());
+      if(adapter != null){
+        List l = viewModel.getIntroductions().getValue();
+        if(l == null){
+          Log.e(TAG, "Introductions list not yet loaded when calling refreshList!");
+          return;
+        }
+        adapter.submitList(getFiltered(viewModel.getIntroductions().getValue(), viewModel.getTextFilter().getValue()));
+      }
+    Trace.endSection();
+  }
+
+  public void onFilterChanged(String filter) {
+    if(adapter != null){
+      viewModel.setTextFilter(filter);
+      List l = viewModel.getIntroductions().getValue();
+      if(l == null){
+        Log.e(TAG, "Introductions list not yet loaded when calling onFilterChanged!");
+        return;
+      }
+      adapter.submitList(getFiltered(viewModel.getIntroductions().getValue(), filter));
+    }
+  }
+
+  /**
+   * Callback if user decides to proceeds with deletion from dialogue.
+   * @param introductionId the introduction to delete.
+   */
+  @Override public void deleteIntroduction(@NonNull Long introductionId) {
+    viewModel.deleteIntroduction(introductionId);
+  }
+
+  /**
+   * Callback if user decides to mask introducer
+   * @param introductionId the introduction for which to mask the introducer.
+   */
+  @Override public void forgetIntroducer(@NonNull Long introductionId) {
+    viewModel.forgetIntroducer(introductionId);
+  }
+
+  private class IntroductionClickListener implements ManageAdapter.InteractionListener  {
+
+    DeleteIntroductionDialog.DeleteIntroduction deleteHandler;
+    ForgetIntroducerDialog.ForgetIntroducer     forgetHandler;
+    Context c;
+
+    public IntroductionClickListener(DeleteIntroductionDialog.DeleteIntroduction d, ForgetIntroducerDialog.ForgetIntroducer f){
+      this.deleteHandler = d;
+      this.forgetHandler = f;
+      c = requireContext();
+    }
+
+    @Override public void accept(@NonNull Long introductionId) {
+      viewModel.acceptIntroduction(introductionId);
+    }
+
+    @Override public void reject(@NonNull Long introductionId) {
+      viewModel.rejectIntroduction(introductionId);
+    }
+
+    @Override public void mask(@NonNull ManageAdapter.IntroductionViewHolder item, String introducerServiceId) {
+      if(!introducerServiceId.equals(TI_Database.UNKNOWN_INTRODUCER_SERVICE_ID)){
+        ForgetIntroducerDialog.show(c, item.getIntroductionId(), item.getIntroduceeName(), item.getIntroducerName(requireContext()), item.getDate(), forgetHandler);
+      }
+    }
+
+    @Override public void delete(@NonNull ManageAdapter.IntroductionViewHolder item, String introducerServiceId) {
+      String introducerName = introducerServiceId.equals(TI_Database.UNKNOWN_INTRODUCER_SERVICE_ID) ? getString(R.string.ManageIntroductionsListItem__Forgotten_Introducer) : Recipient.resolved(TI_Utils.getRecipientIdOrUnknown(introducerServiceId)).getDisplayName(requireContext());
+      DeleteIntroductionDialog.show(c, item.getIntroductionId(), item.getIntroduceeName(), introducerName, item.getDate(), deleteHandler);
+    }
+
+  }
+}
