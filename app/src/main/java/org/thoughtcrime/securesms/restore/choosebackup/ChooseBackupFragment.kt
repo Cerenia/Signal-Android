e6337032a7e753533bb13b7b1318ef2f4e062788
Cerenia <christelle.gloor@inf.ethz.ch>
2024-09-26 13:53:45 +0200

    started fixing backup UI, but stoppen in the middle, GL HF Aditz :)

--- app/src/main/java/org/thoughtcrime/securesms/restore/choosebackup/ChooseBackupFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/restore/choosebackup/ChooseBackupFragment.kt
@@ -5,6 +5,9 @@
 
 package org.thoughtcrime.securesms.restore.choosebackup
 
+// TI_GLUE: eNT9XAHgq0lZdbQs2nfH start
+import android.R
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -16,6 +19,7 @@ import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import org.signal.core.util.logging.Log
 import org.thoughtcrime.securesms.LoggingFragment
@@ -25,7 +29,12 @@ import org.thoughtcrime.securesms.databinding.FragmentChooseBackupBinding
 import org.thoughtcrime.securesms.keyvalue.SignalStore
 import org.thoughtcrime.securesms.registration.fragments.RegistrationViewDelegate
 import org.thoughtcrime.securesms.restore.RestoreViewModel
+import org.thoughtcrime.securesms.trustedIntroductions.glue.ChooseBackupFragmentGlue.OPEN_TI_FILE_REQUEST_CODE
+import org.thoughtcrime.securesms.trustedIntroductions.glue.ChooseBackupFragmentGlue.onChooseTIBackup
 import org.thoughtcrime.securesms.util.navigation.safeNavigate
+
+
+// TI_GLUE: eNT9XAHgq0lZdbQs2nfH end
 
 /**
  * This fragment presents a button to the user to browse their local file system for a legacy backup file.
@@ -47,9 +56,28 @@ class ChooseBackupFragment : LoggingFragment(R.layout.fragment_choose_backup) {
     RegistrationViewDelegate.setDebugLogSubmitMultiTapView(binding.chooseBackupFragmentTitle)
     binding.chooseBackupFragmentButton.setOnClickListener { onChooseBackupSelected() }
 
+    // TI_GLUE: eNT9XAHgq0lZdbQs2nfH start
+    binding.chooseTiBackupFragmentButton.setOnClickListener {onChooseTIBackup(binding.chooseTiBackupFragmentButton, requireContext(), activity) }
+    // TI_GLUE: eNT9XAHgq0lZdbQs2nfH end
+
     binding.chooseBackupFragmentLearnMore.text = HtmlCompat.fromHtml(String.format("<a href=\"%s\">%s</a>", getString(R.string.backup_support_url), getString(R.string.ChooseBackupFragment__learn_more)), 0)
     binding.chooseBackupFragmentLearnMore.movementMethod = LinkMovementMethod.getInstance()
   }
+
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH start
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    val OPEN_FILE_REQUEST_CODE = 3862;
+    val restore: ChooseBackupFragmentDirections.ActionRestore = ChooseBackupFragmentDirections.actionRestore()
+    if (resultCode == Activity.RESULT_OK && data != null) {
+      if (requestCode == OPEN_FILE_REQUEST_CODE) {
+        restore.setUri(data.data)
+      } else if (requestCode == OPEN_TI_FILE_REQUEST_CODE.toInt()) {
+        restore.setTiUri(data.data)
+      }
+      SafeNavigation.safeNavigate(findNavController(requireView()), restore)
+    }
+  }
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH end
 
   private fun onChooseBackupSelected() {
     pickMedia.launch("application/octet-stream")
