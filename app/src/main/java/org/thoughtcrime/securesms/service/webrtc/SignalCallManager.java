8b4ae9a5b634be4bec2955b8db64b4cbea5a6e65
Alex Hart <alex@signal.org>
2024-10-11 14:23:22 -0300

    Add update send job for call link when joining by url.

--- app/src/main/java/org/thoughtcrime/securesms/service/webrtc/SignalCallManager.java
+++ app/src/main/java/org/thoughtcrime/securesms/service/webrtc/SignalCallManager.java
@@ -68,7 +68,6 @@ import org.thoughtcrime.securesms.service.webrtc.state.WebRtcEphemeralState;
 import org.thoughtcrime.securesms.service.webrtc.state.WebRtcServiceState;
 import org.thoughtcrime.securesms.util.AppForegroundObserver;
 import org.thoughtcrime.securesms.util.RecipientAccessList;
-import org.thoughtcrime.securesms.util.RemoteConfig;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
 import org.thoughtcrime.securesms.util.rx.RxStore;
