9a81030c0df27fb31cdaff75858c656d2c0c7bdb
Michelle Tang <mtang@signal.org>
2024-10-17 16:48:06 -0700

    Use custom config for chat folders.

--- app/src/main/java/org/thoughtcrime/securesms/util/RemoteConfig.kt
+++ app/src/main/java/org/thoughtcrime/securesms/util/RemoteConfig.kt
@@ -1091,6 +1091,14 @@ object RemoteConfig {
     hotSwappable = true
   )
 
+  /** Whether or not to show chat folders. */
+  @JvmStatic
+  val showChatFolders: Boolean by remoteBoolean(
+    key = "android.showChatFolders",
+    defaultValue = false,
+    hotSwappable = true
+  )
+
   @JvmStatic
   @get:JvmName("newCallUi")
   val newCallUi: Boolean by remoteBoolean(
