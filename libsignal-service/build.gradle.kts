e30a24521c9bb79af6a279d56b018d6bf032f68e
Cody Henthorne <cody@signal.org>
2024-11-07 10:42:54 -0500

    Update registration for new restore flows.

--- libsignal-service/build.gradle.kts
+++ libsignal-service/build.gradle.kts
@@ -109,6 +109,7 @@ dependencies {
   testImplementation(testLibs.assertk)
   testImplementation(testLibs.conscrypt.openjdk.uber)
   testImplementation(testLibs.mockk)
+  testImplementation(testLibs.hamcrest.hamcrest)
 
   testFixturesImplementation(libs.libsignal.client)
   testFixturesImplementation(testLibs.junit.junit)
