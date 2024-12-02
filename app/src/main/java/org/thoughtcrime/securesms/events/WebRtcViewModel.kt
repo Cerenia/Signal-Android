9c403841422e167674147123a75cf9b3f13366e4
Alex Hart <alex@signal.org>
2024-12-02 13:47:35 -0400

    Split remote user check for group vs individual calls.

--- app/src/main/java/org/thoughtcrime/securesms/events/WebRtcViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/events/WebRtcViewModel.kt
@@ -115,6 +115,13 @@ class WebRtcViewModel(state: WebRtcServiceState) {
     remoteDevicesCount.orElse(0L) > 0L
   }
 
+  @get:JvmName("hasAtLeastOneRemote")
+  val hasAtLeastOneRemote = if (state.callInfoState.callRecipient.isIndividual) {
+    remoteParticipants.isNotEmpty()
+  } else {
+    remoteDevicesCount.orElse(0L) > 0L
+  }
+
   @get:JvmName("shouldRingGroup")
   val ringGroup: Boolean = state.getCallSetupState(state.callInfoState.activePeer?.callId).ringGroup
   val ringerRecipient: Recipient = state.getCallSetupState(state.callInfoState.activePeer?.callId).ringerRecipient
