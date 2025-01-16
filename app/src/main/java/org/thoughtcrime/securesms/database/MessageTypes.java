013b6297bd76c554aa83d39d89d5b530bfefa532
Adrian Cucoș <dielorg@gmail.com>
2025-01-16 07:14:25 +0100

    feat: update `MessageTypes` to keep track of verification mechanism (manual/qr scan/TI)
    - todo: clear those bits when doing an export to normal signal.

--- app/src/main/java/org/thoughtcrime/securesms/database/MessageTypes.java
+++ app/src/main/java/org/thoughtcrime/securesms/database/MessageTypes.java
@@ -122,7 +122,10 @@ public interface MessageTypes {
   long SPECIAL_TYPE_PAYMENTS_TOMBSTONE        = 0x900000000L;
   long SPECIAL_TYPE_BLOCKED                   = 0xA00000000L;
   long SPECIAL_TYPE_UNBLOCKED                 = 0xB00000000L;
-
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH start
+  long SPECIAL_TYPE_IDENTITY_QR_VERIFIED      = 0xC00000000L; // TODO: double check that we don't exceed the Mask
+  long SPECIAL_TYPE_IDENTITY_TI_VERIFIED      = 0xD00000000L;
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH end
   long IGNORABLE_TYPESMASK_WHEN_COUNTING = END_SESSION_BIT | KEY_EXCHANGE_IDENTITY_UPDATE_BIT | KEY_EXCHANGE_IDENTITY_VERIFIED_BIT;
 
   static boolean isStoryReaction(long type) {
@@ -259,6 +262,20 @@ public interface MessageTypes {
   static boolean isIdentityVerified(long type) {
     return (type & KEY_EXCHANGE_IDENTITY_VERIFIED_BIT) != 0;
   }
+
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH start
+  static boolean isIdentityQrVerified(long type) {
+    return isIdentityVerified(type) && ((type & SPECIAL_TYPES_MASK) == SPECIAL_TYPE_IDENTITY_QR_VERIFIED);
+  }
+
+  static boolean isIdentityTiVerified(long type) {
+    return isIdentityVerified(type) && ((type & SPECIAL_TYPES_MASK) == SPECIAL_TYPE_IDENTITY_TI_VERIFIED);
+  }
+
+  static boolean isDuplexVerified(long type) {
+    return isIdentityQrVerified(type) && isIdentityTiVerified(type);
+  }
+  // TI_GLUE: eNT9XAHgq0lZdbQs2nfH end
 
   static boolean isIdentityDefault(long type) {
     return (type & KEY_EXCHANGE_IDENTITY_DEFAULT_BIT) != 0;
